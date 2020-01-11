068601628f1050f73c27b23b496104d39c1f6058
ruiponte1990 <ruiponte1990@gmail.com>
2020-01-10 21:54:20 -0500

    echo

--- sendFile.sh
+++ sendFile.sh
@@ -2,7 +2,7 @@ ARCHIVE="${2}"
 ARCHIVE_SIZE=`cat "${ARCHIVE}" | wc --bytes`
 
 if [[${ARCHIVE_SIZE} -le 104875]]; then
-	aws glacier upload-archive --account-id - --vault-name "${1}" --body "${2}" --archive-description "${2}"
+	echo aws glacier upload-archive --account-id - --vault-name "${1}" --body "${2}" --archive-description "${2}"
 else
-	sh multipart.sh "${1}" "${2}"
+	echo sh multipart.sh "${1}" "${2}"
 fi
